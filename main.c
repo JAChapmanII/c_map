1c998aecce0cb4953f2768357da9175b949cc4ae
Jeff Chapman II <jac@JAChapmanII.net>
2011-06-19 10:55:30 -0400

    Add deconstructor and find node function

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ typedef struct bMapNode {
 } bMap;
 
 bMap *consBMap(char* k, char* v) {
+	if(!k || !v) return NULL;
 	bMap *bm = malloc(sizeof(bMap));
 	if(bm) {
 		bm->left = NULL; bm->right = NULL;
@@ -21,9 +22,34 @@ bMap *consBMap(char* k, char* v) {
 	return bm;
 }
 
+void deconsBMap(bMap *bm) {
+	if(!bm) return;
+	deconsBMap(bm->left);
+	deconsBMap(bm->right);
+	free(bm->key);
+	free(bm->val);
+}
+
+bMap *findNode(bMap *bm, char* k) {
+	if(!bm || !k) return NULL;
+	int cmp = strcmp(bm->key, k);
+	if(!cmp)
+		return bm;
+	if(cmp < 0)
+		return findNode(bm->left, k);
+	return findNode(bm->right, k);
+}
+
 int main(int argc, char **argv) {
 	bMap *bm = consBMap("a", ".-");
 	printf("Hello, world!\n");
 
+	bMap *n = findNode(bm, "a");
+	if(!n)
+		printf("Not found");
+	else
+		printf("Value of bm[\"a\"]: %s\n", n->val);
+
+	deconsBMap(bm);
 	return 0;
 }
